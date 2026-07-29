6055fa0f2a3e6edbec7a41d5fa2fcb97e5346aa9
Martin Petrov <martin_petrov@me.com>
2026-07-29 08:35:22 +0300

    improve getEnvName script util

--- scripts/utils/get-env-name.ts
+++ scripts/utils/get-env-name.ts
@@ -1,10 +1,24 @@
-export function getEnvName() {
-  const envName = Deno.args[0] ?? "staging";
+type Env = typeof envs[number];
 
-  if (envName !== "prod" && envName !== "staging") {
-    console.error(
-      `Error: Invalid environment '${envName}'. Must be 'prod' or 'staging'.`,
-    );
+const envs = ["prod", "staging"] as const;
+const defaultEnv = "staging" satisfies Env;
+
+function isEnv(value: unknown): value is Env {
+  return envs.includes(value as Env);
+}
+
+const listFormat = new Intl.ListFormat("en", {
+  style: "long",
+  type: "disjunction",
+});
+
+export function getEnvName() {
+  const envName = Deno.args[0] ?? defaultEnv;
+
+  if (!isEnv(envName)) {
+    const envList = listFormat.format(envs.map((it) => `'${it}'`));
+    const msg = `Error: Invalid environment '${envName}'. Must be ${envList}.`;
+    console.error(msg);
     Deno.exit(1);
   }
 
