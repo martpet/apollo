20496b0a37f693744cbb05937a06a5cae4a75a33
Martin Petrov <martin_petrov@me.com>
2026-07-29 07:47:59 +0300

    rename "stage" to "staging"

--- scripts/utils/get-env.ts
+++ scripts/utils/get-env.ts
@@ -1,9 +1,9 @@
 export function getEnvArg() {
-  const env = Deno.args[0] ?? "stage";
+  const env = Deno.args[0] ?? "staging";
 
-  if (env !== "prod" && env !== "stage") {
+  if (env !== "prod" && env !== "staging") {
     console.error(
-      `Error: Invalid environment '${env}'. Must be 'prod' or 'stage'.`,
+      `Error: Invalid environment '${env}'. Must be 'prod' or 'staging'.`,
     );
     Deno.exit(1);
   }
